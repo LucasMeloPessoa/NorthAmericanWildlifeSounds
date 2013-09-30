32adac0612e2387bddb980e7adbea34770faf17e
HyruleanHero1988 <hyruleanhero1988@gmail.com>
2013-09-30 14:13:43 -0400

    Copied in previous code into the GIT repository

--- src/com/example/northamericanwildlifesounds/Intro.java
+++ src/com/example/northamericanwildlifesounds/Intro.java
@@ -1,7 +1,10 @@
 package com.example.northamericanwildlifesounds;
 
+import com.example.northamericanwildlifesounds.R;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class Intro extends Activity {
@@ -10,6 +13,20 @@ public class Intro extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_intro);
+		Thread timer = new Thread(){
+			public void run(){
+				try{
+					sleep(2000);
+				}catch(InterruptedException e){
+					e.printStackTrace();
+				}finally{
+					Intent openStartPoint = new Intent("com.example.wildlifesounds_version2.MAINACTIVITY");
+					startActivity(openStartPoint);//when this is called, on pause is called after
+				}
+			}
+		};
+		timer.start();
+
 	}
 
 	@Override
@@ -19,4 +36,11 @@ public class Intro extends Activity {
 		return true;
 	}
 
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
 }
+
+
--- src/com/example/northamericanwildlifesounds/Main.java
+++ src/com/example/northamericanwildlifesounds/Main.java
@@ -1,22 +1,72 @@
 package com.example.northamericanwildlifesounds;
 
+import com.example.northamericanwildlifesounds.R;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewSwitcher;
 
 public class Main extends Activity {
 
+	private Button learn, play;
+	private ViewSwitcher switcher;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initializeVariable();   // call this method to initialize variables
+		learn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent animallist= new Intent("com.example.northamericanwildlifesounds.ANIMALLIST");
+				startActivity(animallist);
+			}
+		});
+	
+		play.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	
+	
 	}
 
+
+	
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+/**
+ * initializeVariable method initialize variables
+ * 
+ */
+	private void initializeVariable(){
+		
+		learn=(Button)findViewById(R.id.learnButton);
+		play=(Button)findViewById(R.id.playButton);
+		
+	}
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
 }
