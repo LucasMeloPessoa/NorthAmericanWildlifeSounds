fc161adc59d86f306e6f918ecb6e6b8a22f237d4
atlantaboix <djdangie@yahoo.com>
2013-10-02 12:30:24 -0400

    -change type for sound and imageURL in Animal.java -set up list view in Animallist.java

--- src/com/example/northamericanwildlifesounds/Animal.java
+++ src/com/example/northamericanwildlifesounds/Animal.java
@@ -1,19 +1,24 @@
 package com.example.northamericanwildlifesounds;
 
 
+
 public class Animal {
 
-	 private String name, imageURL, sound, type;
+	 private String name, type;
+	 private int sound, imageURL;
+	 
+
 	 
 	 
 	 
 	//Constructor that initialize all variables
-	public Animal( String name, String imageURL, String sound, String type){
+	public Animal( String name, int imageURL, int sound, String type){
 		this.name=name;
 		this.imageURL=imageURL;
 		this.sound=sound;
 		this.type=type;
-		
+	
+	
 	}
 	
 	/*return Animal Name*/
@@ -21,11 +26,11 @@ public class Animal {
 		return name;
 	}
 	/*return Animal ImageURL*/
-	public String getAnimalImageURL(){
+	public int getAnimalImageURL(){
 		return imageURL;
 	}
 	/*return Animal sound*/
-	public String getAnimalsound(){
+	public int getAnimalsound(){
 		return sound;
 	}
 	/*return Animal type*/
@@ -39,11 +44,11 @@ public class Animal {
 		name=input;
 	}
 	/*add Animal sound mp3 is raw folder*/
-	public void addAnimalSound( String input){
+	public void addAnimalSound( int input){
 		sound=input;
 	}	
 	/*add Animal imageURL that contain in drawable folder*/
-		public void addAnimalImageURL( String input){
+		public void addAnimalImageURL( int input){
 		imageURL=input;
 	}
 	/*add Animal type */
--- src/com/example/northamericanwildlifesounds/Animallist.java
+++ src/com/example/northamericanwildlifesounds/Animallist.java
@@ -1,75 +1,93 @@
 package com.example.northamericanwildlifesounds;
+
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.widget.Button;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import java.util.ArrayList;
 
 import com.example.northamericanwildlifesounds.R;
 
-
-
-
-public class Animallist extends Activity {
-	private ArrayList<Animal> animalList_database; 	//contain a array list of Animal class
+public class Animallist extends ListActivity {
+	private ArrayList<Animal> animal_database; // contain a array list of Animal
+												// class
 	private ListView animallist;
-	
-	private String[]animals= {"bobcat","coyote","deer","fox","mountainlion","possum",
-							  "rabbit","raccoon","squirrel","turkey"};
-	
-	
 
-/*reorder function
- * @param take in the list of animal name to be shorted based on A-Z alphabet. 
- * @return ordered animal name
- */
-	private String[] reorder(String[]animal){
-		//implement your code here
-		return null;
-	}
-	
-	
-	
-	/*addToAnimalListDatabase function
-	 * @param take in the list of animal name and create a new animal class for each animal.
-	 *       for each animal class:
-	 *       	initialize animal sound,imageURL,sound, and type variable
-	 *        once all the animal class is created, add them to animalList_database.
-	 * 
-	 */
-	private void addToAnimalListDatabase(String[]animal){
-		//implement your code here	
-			
-	}
-		
-	
-	
-	
+	private String[] animalClass = { "bobcat", "coyote", "deer", "fox",
+			"mountainlion", "possum", "rabbit", "raccoon", "squirrel", "turkey" };
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_animallist);
-	
+		// setContentView(R.layout.activity_animallist);
+		setListAdapter(new ArrayAdapter<String>(Animallist.this,
+				android.R.layout.simple_list_item_1, animalClass));
+
+	}
+
+	/**
+	 * @Override public boolean onCreateOptionsMenu(Menu menu) { // Inflate the
+	 *           menu; this adds items to the action bar if it is present.
+	 *           getMenuInflater().inflate(R.menu.animallist, menu); return
+	 *           true; }
+	 **/
+
+	private void initializeVariable() {
+
+	}
+
+	/*
+	 * reorder function
+	 * 
+	 * @param take in the list of animal name to be shorted based on A-Z
+	 * alphabet.
+	 * 
+	 * @return ordered animal name
+	 */
+	private String[] reorder(String[] animal) {
+		// implement your code here
+		return null;
+	}
+
+	/*
+	 * addToAnimalListDatabase function
+	 * 
+	 * @param take in the list of animal name and create a new animal class for
+	 * each animal. for each animal class: initialize animal
+	 * sound,imageURL,sound, and type variable once all the animal class is
+	 * created, add them to animalList_database.
+	 */
+	private void addToAnimalListDatabase(String[] animal) {
+		// implement your code here
+
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.animallist, menu);
-		return true;
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
 	}
-	
-	
-	
-	private void initializeVariable(){
-		
-		
-	
 
-		
+	/**
+	 * this onListItemClick method will be called when user click on any item on
+	 * the list above
+	 **/
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		String clickedAnimal = animalClass[position]; // store animal name that
+														// user click
+		try {
+			Class ourClass = Class
+					.forName("com.example.northamericanwildlifesounds.AnimalListSub");
+
+			Intent ourIntent = new Intent(Animallist.this, ourClass); 
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
-
-
