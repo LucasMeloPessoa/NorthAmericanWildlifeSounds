d91db96a8e4babce0a575bf2c06b53e587c4e2c2
djdangie@yahoo.com <newuser@unknown68a86d4c1d86>
2013-10-04 14:26:14 -0400

    updating project

--- src/com/example/northamericanwildlifesounds/Animal.java
+++ src/com/example/northamericanwildlifesounds/Animal.java
@@ -1,81 +1,45 @@
 package com.example.northamericanwildlifesounds;
 
 
-import java.util.ArrayList;
-
-import android.app.Activity;
-
-import com.example.northamericanwildlifesounds.R;
-public class Animal extends Activity {
-
-	 private String name;
-	 private int sound, imageURL;
-	 ArrayList<Animal> type;
-	 
-	 
-	 
-	 
-	 
-	//Constructor that initialize all variables
-	public Animal( String name, int imageURL, int sound ){
+public class Animal {
+	private  String name;
+	private int sound,imageURL;
+	
+	
+	//constructor
+	public Animal(String name, int soundfile, int image){
 		this.name=name;
-		this.imageURL=imageURL;
-		this.sound=sound;
-		type= new ArrayList<Animal>();
-		
-		
+		sound=soundfile;
+		imageURL=image;
 	}
 	
-	/*return Animal Name*/
-	public String getAnimalName(){
+
+	
+	public void addTypeName(String input){
+		if (input!=null){
+			name=input;
+		}
+	}
+	
+	public void addSound(int input){
+			sound=input;
+	}
+	
+	public void addImageURL(int input){
+		imageURL=input;
+}
+
+
+	public String getName(){
 		return name;
 	}
-	/*return Animal ImageURL*/
-	public int getAnimalImageURL(){
-		return imageURL;
-	}
-	/*return Animal sound*/
-	public int getAnimalsound(){
+	
+	public int getSound(){
 		return sound;
 	}
-	/*return Animal type*/
-	public ArrayList<Animal> getType(){
-		return type;
-	}
 	
-	
-	/*add Animal Name*/
-	public void addAnimalName( String input){
-		name=input;
-	}
-	/*add Animal sound mp3 is raw folder*/
-	public void addAnimalSound(int file){
-		sound=file;
-		
-	}	
-	/*add Animal imageURL that contain in drawable folder*/
-		public void addAnimalImageURL( int input){
-		imageURL=input;
-	}
-	public boolean containSound(int file){
-			if(sound==file){
-				return true;
-			}
-			else
-				return false;
-	}
-	public boolean containImageURL(int file){
-		if(imageURL==file){
-			return true;
-		}
-		else
-			return false;
-}
-	
-	/*add Animal type */
-	public void addAnimalType( String name, int sound, int img){
-		//check see if type already exist, if not, add it.
-		//if(add name
+	public int getImageURL(){
+		return imageURL;
 	}
 	
 }
--- src/com/example/northamericanwildlifesounds/Animallist.java
+++ src/com/example/northamericanwildlifesounds/Animallist.java
@@ -1,94 +1,130 @@
 package com.example.northamericanwildlifesounds;
 
-import android.os.Bundle;
-import android.app.ListActivity;
-import android.content.Intent;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
+
+
 
 import com.example.northamericanwildlifesounds.R;
 
-public class Animallist extends ListActivity {
-	private ArrayList<Animal> animal_database; // contain a array list of Animal
-												// class
-	private ListView animallist;
+public class Animallist extends Activity {
 
-	private String[] animalClass = { "bobcat", "coyote", "deer", "fox",
-			"mountainlion", "possum", "rabbit", "raccoon", "squirrel", "turkey" };
+	private ArrayList<Animal> list;										
+	private String[] animalCategory;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		// setContentView(R.layout.activity_animallist);
-		setListAdapter(new ArrayAdapter<String>(Animallist.this,
-				android.R.layout.simple_list_item_1, animalClass));
+	HashMap<String, List<Integer>> sound, imageURL;
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String>animalHeader;
+    HashMap<String, List<Animal>> animalChild;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animallist);
+ 
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.elvAnimallist);
+ 
+        // preparing list data
+        initializeVariables();
+        listAdapter = new ExpandableListAdapter(this, animalHeader, animalChild);
+ 
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+    }
+ 
 
+    private void initializeVariables() {
+        animalHeader = new ArrayList<String>();
+        animalChild = new HashMap<String, List<Animal>>();
+        
+        //INSERT MAIN ANIMAL HERE
+        animalCategory=new String[]{ "bobcat", "coyote", "deer", "fox",
+    			"mountainlion", "possum", "rabbit", "raccoon", "squirrel", 
+    			"turkey","duck","elephant","chicken" };
+        
+       
+        // Adding Animal Head data
+        animalCategory=sortStringArray(animalCategory);	//to be sorted.
+   		for(int i=0;i<animalCategory.length;i++){
+        animalHeader.add(animalCategory[i]);
+   		}
 
-	}
+ 
+        // Adding child data
+   		List<Animal>deer = new ArrayList<Animal>();
+   		deer.add(new Animal("Doe Grunt",40, 20));
+   		deer.add(new Animal("Buck Grunt",40, 20));
+   		deer.add(new Animal("Buck Bawl",40, 20));
+   		deer.add(new Animal("Contact Call",40, 20));
+   	
+   		List<Animal>bobcat = new ArrayList<Animal>();
+   		bobcat.add(new Animal("Bobcat Female",40, 20));
+   	
+   		List<Animal>duck = new ArrayList<Animal>();
+   		duck.add(new Animal("Duck Quack",40, 20));
+   		duck.add(new Animal("Duck Swimming",30,10));
+ 
 
-	/**
-	 * @Override public boolean onCreateOptionsMenu(Menu menu) { // Inflate the
-	 *           menu; this adds items to the action bar if it is present.
-	 *           getMenuInflater().inflate(R.menu.animallist, menu); return
-	 *           true; }
-	 **/
+ 
+        animalChild.put("deer", deer); // Header, Child data
+        animalChild.put("bobcat", bobcat);
+        animalChild.put("duck", duck);
+        
+    }
 
-	private void initializeVariable() {
+    
+    
+    
+    
+    
+    
+    private HashMap<String, List<Integer>> setImageURLData(String[] file) {
+    	//IMPLEMENT YOUR CODE HERE
+    	HashMap<String, List<Integer>> data=new HashMap<String, List<Integer>>() ;
+    	
+    	return data;
+    }
+      
+    
+    
+    private HashMap<String, List<Integer>> setSoundData(String[] file) {
+    	//IMPLEMENT YOUR CODE HERE
+    	HashMap<String, List<Integer>> data=new HashMap<String, List<Integer>>() ;
+    	
+    	return data;
+    }
+    
+    
+    
+    
+   /**
+    * 
+    * @param file to be sorted in alphabet letter
+    * return sorted array
+    */
+    
+    private String[] sortStringArray(String[] file) {
+    	//IMPLEMENT YOUR CODE HERE
+    	String[]data=file;
+    	
+    	
+    	return data;
+    }
+    
+    
 
-	}
-
-	/*
-	 * reorder function
-	 * 
-	 * @param take in the list of animal name to be shorted based on A-Z
-	 * alphabet.
-	 * 
-	 * @return ordered animal name
-	 */
-	private String[] reorder(String[] animal) {
-		// implement your code here
-		return null;
-	}
-
-	/*
-	 * addToAnimalListDatabase function
-	 * 
-	 * @param take in the list of animal name and create a new animal class for
-	 * each animal. for each animal class: initialize animal
-	 * sound,imageURL,sound, and type variable once all the animal class is
-	 * created, add them to animalList_database.
-	 */
-	private void addToAnimalListDatabase(String[] animal) {
-		// implement your code here
-
-	}
-
-	@Override
-	protected void onPause() {
-		// TODO Auto-generated method stub
-		super.onPause();
-		finish();
-	}
-
-	/**
-	 * this onListItemClick method will be called when user click on any item on
-	 * the list above
-	 **/
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		// TODO Auto-generated method stub
-		super.onListItemClick(l, v, position, id);
-		String clickedAnimal = animalClass[position]; // store animal name that
-														// user click
-		try {
-			Class ourClass = Class
-					.forName("com.example.northamericanwildlifesounds.AnimalListSub");
-
-			Intent ourIntent = new Intent(Animallist.this, ourClass); 
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
 
 }
