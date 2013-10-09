715c3ffeeaf82ff9137baf467d14040ec35f14fc
HyruleanHero1988 <hyruleanhero1988@gmail.com>
2013-10-09 15:59:12 -0400

    Adding sounds and images that were lost in the last commit.

--- gen/com/example/northamericanwildlifesounds/R.java
+++ gen/com/example/northamericanwildlifesounds/R.java
@@ -21,9 +21,18 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int buck_grunt=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int mainlogo=0x7f020002;
+        public static final int bobcat=0x7f020000;
+        public static final int buck_grunt=0x7f020001;
+        public static final int coyote=0x7f020002;
+        public static final int fox=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int mainlogo=0x7f020005;
+        public static final int mountain_lion=0x7f020006;
+        public static final int possum=0x7f020007;
+        public static final int rabbit=0x7f020008;
+        public static final int raccoon=0x7f020009;
+        public static final int squirrel=0x7f02000a;
+        public static final int turkey=0x7f02000b;
     }
     public static final class id {
         public static final int action_settings=0x7f09000b;
